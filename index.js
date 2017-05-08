5565958cacd458f6dad20d0f3c658728232ad7ee
Khalid Merhi <kmerhi@shinydocs.com>
2017-05-07 21:23:44 -0400

    Minor refactor for easier testing

--- index.js
+++ index.js
@@ -14,16 +14,9 @@ program
 	.option('-u, --username <username>', 'user to connect to Artifactory repo')
 	.option('-p, --password <password>', 'password (or API key) for basic auth')
 	.action(uri => {
-		let {
-			username,
-			password
-		} = program;
+		const creds = getCreds(program);
 
-		if (username && username.indexOf(':') > 0) {
-			[username, password] = username.split(':')
-		}
-
-		getDownloadUri(uri, username, password)
+		getDownloadUri(uri, creds)
 			.then(url => {
 				console.log(url);
 				process.exit(0);
@@ -38,13 +31,30 @@ program
 	})
 	.parse(process.argv);
 
+function getCreds(program) {
+	let {
+		username,
+		password
+	} = program;
+
+	if (username && username.indexOf(':') > 0) {
+		[username, password] = username.split(':')
+	}
+
+	return {
+		username,
+		password
+	}
+}
+
 async function fetchUri(uri, username, password) {
-	const response = await fetch(uri, {
+	const options = {
 		method: 'get',
 		headers: {
 			'Authorization': 'Basic ' + base64.encode(username + ':' + password)
 		},
-	});
+	};
+	const response = await fetch(uri, options);
 	const body = await response.json();
 
 	if (response.status !== 200) {
@@ -54,22 +64,42 @@ async function fetchUri(uri, username, password) {
 	return body;
 }
 
-async function getDownloadUri(uri, username, password) {
+async function getDownloadUri(uri, creds) {
+	const {
+		username,
+		password
+	} = creds;
+
 	const json = await fetchUri(uri, username, password);
 	let children = json.children;
 
 	if (children) {
-		children = children.filter(child => {
-			return !child.uri.endsWith('.pom') && !child.uri.endsWith('.xml'); // return child.folder
-		}).sort((o1, o2) => naturalSort({
-			direction: 'desc'
-		})(o1.uri, o2.uri));
-
-		// Correct sorting will bring the newest resource to the top
-		const nextResource = children[0];
-
-		return await getDownloadUri(uri + nextResource.uri, username, password);
+		const nextUri = getNextUri(uri, children);
+		return await getDownloadUri(nextUri, creds);
 	} else {
 		return json.downloadUri;
 	}
+}
+
+function getNextUri(uri, children) {
+	// Proper natural sorting will bring the newest resource to the top
+	children = getSortedChildren(children);
+	const nextResource = children[0];
+
+	return uri + nextResource.uri
+}
+
+function getSortedChildren(children) {
+	return children.filter(nonPomOrXmlUri).sort(naturalSortUri);
+}
+
+function nonPomOrXmlUri(object) {
+	return !object.uri.endsWith('.pom') &&
+		!object.uri.endsWith('.xml');
+}
+
+function naturalSortUri(o1, o2) {
+	return naturalSort({
+		direction: 'desc'
+	})(o1.uri, o2.uri)
 }
