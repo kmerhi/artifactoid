db6b0960250a04350b72519da9dd56519c663166
Khalid Merhi <kmerhi@shinydocs.com>
2017-05-08 03:31:02 -0400

    Improve test coverage of utils

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,8 +1,8 @@
 const utils = require('./index');
 
-describe('getCredentials', () => {
+describe('utils', () => {
 
-	it('should handle no credentials passed', () => {
+	it('should getCredentials when no credentials passed', () => {
 		const actual = utils.getCredentials();
 		const expected = {
 			username: undefined,
@@ -12,7 +12,7 @@ describe('getCredentials', () => {
 		expect(actual).toEqual(expected);
 	});
 
-	it('should handle concatenated input', () => {
+	it('should getCredentials when concatenated input', () => {
 		const actual = utils.getCredentials({
 			username: 'potato:secret'
 		});
@@ -20,6 +20,19 @@ describe('getCredentials', () => {
 			username: 'potato',
 			password: 'secret'
 		};
+
+		expect(actual).toEqual(expected);
+	});
+
+	it('should getNextUri when array of objects provided', () => {
+		const children = [
+			{ uri: '/1.2.1-SNAPSHOT' },
+			{ uri: '/1.2.2-SNAPSHOT' },
+			{ uri: '/1.2.10-SNAPSHOT' },
+            { uri: '/1.2.11-SNAPSHOT' }
+		];
+		const actual = utils.getNextUri('http://www.potatoes.com/api/storage', children);
+		const expected = 'http://www.potatoes.com/api/storage/1.2.11-SNAPSHOT';
 
 		expect(actual).toEqual(expected);
 	});
