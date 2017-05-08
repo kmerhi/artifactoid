5dd63c7a24309857485c8a1dbd35ae082e6b3cca
Khalid Merhi <khalid.merhi@gmail.com>
2017-05-08 03:45:49 -0400

    Minor

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -29,7 +29,7 @@ describe('utils', () => {
 			{ uri: '/1.2.1-SNAPSHOT' },
 			{ uri: '/1.2.2-SNAPSHOT' },
 			{ uri: '/1.2.10-SNAPSHOT' },
-            { uri: '/1.2.11-SNAPSHOT' }
+			{ uri: '/1.2.11-SNAPSHOT' }
 		];
 		const actual = utils.getNextUri('http://www.potatoes.com/api/storage', children);
 		const expected = 'http://www.potatoes.com/api/storage/1.2.11-SNAPSHOT';
